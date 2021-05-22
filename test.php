1b878564dff471c73281aa4af691cf7ab1b94ccb
Daisuke Sasaki <gerund25495641@gmail.com>
2021-05-22 18:09:51 +0900

    modified the code

---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>無題ドキュメント</title>
+<style>
+    table {
+      border-collapse: collapse;
+    }
+    td {
+      border: 1px solid black;
+    }
+    .noborder {
+      border: 0px none;
+    }
+  </style>
+</head>
+<body>
+	<table class="table1">
+    <tr>
+      <td>1行1列</td>
+      <td class="noborder">1行2列</td>
+    </tr>
+    <tr>
+      <td>2行1列</td>
+      <td>2行2列</td>
+    </tr>
+  </table>
+	
+	
+</body>
+</html>
+?>
